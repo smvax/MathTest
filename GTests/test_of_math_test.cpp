2dcac56aa8a07a16f2f391bc2ae0fd491d7ad54e
smvax <sharov8055@gmail.com>
2026-09-16 23:21:51 +0300

    Добавлена реализация MathTest + покрытие тестаии

--- GTests/test_of_math_test.cpp
+++ GTests/test_of_math_test.cpp
@@ -2,71 +2,29 @@
 #include "math_test.h"
 #include <cstdlib>
 
-TEST(TaskMathTest, TestInitFields) {
-    Task t1(12, 4, '/');
+TEST(TaskTest, TestInitConsFields) {
+    Task t1(12, 4, '*');
     EXPECT_EQ(t1.num_1, 12);
     EXPECT_EQ(t1.num_2, 4);
-    EXPECT_EQ(t1.operation, '/');
+    EXPECT_EQ(t1.operation, '*');
 }
 
-TEST(TaskMathTest, TestAddPositive) {
-    Task t1(5, 10, '+');
-    EXPECT_DOUBLE_EQ(t1.answer, 15.0);
+TEST(TaskTest, TestBadInitConsFields) {
+    ASSERT_THROW(Task t1(12, 4, 'j'), std::invalid_argument);
 }
 
-TEST(TaskMathTest, TestAddNegative) {
-    Task t1(-5, -10, '+');
-    EXPECT_DOUBLE_EQ(t1.answer, -15.0);
-}
-
-TEST(TaskMathTest, TestSubPositiveResult) {
-    Task t1(15, 5, '-');
-    EXPECT_DOUBLE_EQ(t1.answer, 10.0);
-}
-
-TEST(TaskMathTest, TestSubNegativeResult) {
-    Task t1(5, 15, '-');
-    EXPECT_DOUBLE_EQ(t1.answer, -10.0);
-}
-
-TEST(TaskMathTest, TestMultDifferentSigns) {
-    Task t1(-4, 5, '*');
-    EXPECT_DOUBLE_EQ(t1.answer, -20.0);
-}
-
-TEST(TaskMathTest, TestDivIntegerPositive) {
-    Task t1(10, 2, '/');
-    EXPECT_DOUBLE_EQ(t1.answer, 5.0);
-}
-
-TEST(TaskMathTest, TestDivIntegerNegative) {
-    Task t1(10, -2, '/');
-    EXPECT_DOUBLE_EQ(t1.answer, -5.0);
-}
-
-TEST(TaskMathTest, TestDivFractionalPositive) {
-    Task t1(5, 2, '/');
-    EXPECT_DOUBLE_EQ(t1.answer, 2.5);
-}
-
-TEST(TaskMathTest, TestDivNegativeFractional) {
-    Task t1(-5, 2, '/');
-    EXPECT_DOUBLE_EQ(t1.answer, -2.5);
-}
-
-TEST(TaskMathTest, RangeDefault) {
+TEST(TaskTest, TestBaseConsRange) {
     for (int i = 0; i < 1000; ++i) {
         Task t1;
         EXPECT_GE(t1.num_1, -100);
         EXPECT_LE(t1.num_1, 100);
         EXPECT_GE(t1.num_2, -100);
         EXPECT_LE(t1.num_2, 100);
-        EXPECT_TRUE(t1.operation == '+' || t1.operation == '-' ||
-            t1.operation == '*' || t1.operation == '/');
+        EXPECT_TRUE(t1.operation == '+' || t1.operation == '-' || t1.operation == '*');
     }
 }
 
-TEST(TaskMathTest, TestRangeCustom) {
+TEST(TaskTest, TestInitConsRange) {
     int min_v = 10;
     int max_v = 20;
     for (int i = 0; i < 1000; ++i) {
@@ -75,37 +33,124 @@ TEST(TaskMathTest, TestRangeCustom) {
         EXPECT_LE(t1.num_1, max_v);
         EXPECT_GE(t1.num_2, min_v);
         EXPECT_LE(t1.num_2, max_v);
+        EXPECT_TRUE(t1.operation == '+' || t1.operation == '-' || t1.operation == '*');
     }
 }
 
-TEST(TaskMathTest, TestRangeSinglePoint) {
+TEST(TaskTest, TestAddPositive) {
+    Task t1(5, 10, '+');
+    EXPECT_EQ(t1.answer, 15);
+}
+
+TEST(TaskTest, TestAddNegative) {
+    Task t1(-5, -10, '+');
+    EXPECT_EQ(t1.answer, -15);
+}
+
+TEST(TaskTest, TestSubPositiveResult) {
+    Task t1(15, 5, '-');
+    EXPECT_EQ(t1.answer, 10);
+}
+
+TEST(TaskTest, TestSubNegativeResult) {
+    Task t1(5, 15, '-');
+    EXPECT_EQ(t1.answer, -10);
+}
+
+TEST(TaskTest, TestMultDifferentSigns) {
+    Task t1(-4, 5, '*');
+    EXPECT_EQ(t1.answer, -20);
+}
+
+TEST(TaskTest, TestRangeSinglePoint) {
     Task t1(5, 5);
     EXPECT_EQ(t1.num_1, 5);
     EXPECT_EQ(t1.num_2, 5);
 }
 
-TEST(TaskMathTest, TestZeroDefensePositive) {
-    for (int i = 0; i < 1000; ++i) {
-        Task t1(0, 5);
-        if (t1.operation == '/') {
-            EXPECT_NE(t1.num_2, 0);
-        }
+TEST(MathTestTest, TestConstructorCount) {
+    MathTest test(100);
+    EXPECT_EQ(test.getCounts(), 100);
+    EXPECT_EQ(test.getCorrectCount(), 0);
+
+    for (int i = 0; i < 100; ++i) {
+        Task t = test.getTask(i);
+        EXPECT_GE(t.num_1, -100);
+        EXPECT_LE(t.num_1, 100);
     }
 }
 
-TEST(TaskMathTest, TestZeroDefenseNegative) {
-    for (int i = 0; i < 1000; ++i) {
-        Task t1(-5, 0);
-        if (t1.operation == '/') {
-            EXPECT_NE(t1.num_2, 0);
-        }
+TEST(MathTestTest, TestConstructorCountBad) {
+    ASSERT_THROW(MathTest test(-1), std::invalid_argument);
+}
+
+TEST(MathTestTest, TestConstructorRange) {
+    MathTest test(100, 10, 20);
+    EXPECT_EQ(test.getCounts(), 100);
+
+    for (int i = 0; i < 100; ++i) {
+        Task t = test.getTask(i);
+        EXPECT_GE(t.num_1, 10);
+        EXPECT_LE(t.num_1, 20);
     }
 }
 
-TEST(TaskMathTest, TestZeroDefenseAtZero) {
-    for (int i = 0; i < 1000; ++i) {
-        Task t1(0, 0);
-        EXPECT_NE(t1.operation, '/'); //замена деления на +
-        EXPECT_DOUBLE_EQ(t1.answer, 0.0);
+TEST(MathTestTest, TestConstructorRangeBad) {
+    ASSERT_THROW(MathTest test(-1, 10, 20), std::invalid_argument);
+}
+ 
+TEST(MathTestTest, TestConstructorFixedOp) {
+    MathTest test(100, 1, 10, '*');
+    EXPECT_EQ(test.getCounts(), 100);
+
+    for (int i = 0; i < 100; ++i) {
+        Task t = test.getTask(i);
+        EXPECT_EQ(t.operation, '*');
+        EXPECT_EQ(t.answer, t.num_1 * t.num_2);
     }
+}
+
+TEST(MathTestTest, TestConstructorFixedOpBad) {
+    ASSERT_THROW(MathTest test(-1, 10, 20, '+'), std::invalid_argument);
+    ASSERT_THROW(MathTest test(1, 10, 20, 'f'), std::invalid_argument);
+}
+
+TEST(TaskMathTest, TestMathTestSubmit) {
+    MathTest test(3);
+
+    test.setTask(0, Task(5, 5, '+'));  //10
+    test.setTask(1, Task(10, 4, '-')); //6
+    test.setTask(2, Task(3, 4, '*'));  //12
+
+    test.submitAnswer(0, 10); //+
+    test.submitAnswer(1, 99); //-
+    test.submitAnswer(2, 12); //+
+
+    EXPECT_EQ(test.getCorrectCount(), 2);
+    EXPECT_EQ(test.getUserAnswer(0), 10);
+    EXPECT_EQ(test.getUserAnswer(1), 99);
+}
+
+TEST(TaskMathTest, TestMathTestMark) {
+    MathTest test(10);
+
+    for (int i = 0; i < 10; ++i) {
+        test.setTask(i, Task(1, 1, '+')); //2
+    }
+
+    for (int i = 0; i < 10; ++i) test.submitAnswer(i, 2);
+    EXPECT_EQ(test.calculateMark(), 'A');
+
+    MathTest testB(10);
+    for (int i = 0; i < 10; ++i) testB.setTask(i, Task(1, 1, '+'));
+    for (int i = 0; i < 8; ++i)  testB.submitAnswer(i, 2);  //8+
+    for (int i = 8; i < 10; ++i) testB.submitAnswer(i, 99); //2-
+    EXPECT_EQ(testB.calculateMark(), 'B');
+
+    MathTest testF(10);
+    for (int i = 0; i < 10; ++i) testF.setTask(i, Task(1, 1, '+'));
+
+    for (int i = 0; i < 5; ++i)  testF.submitAnswer(i, 2);
+    for (int i = 5; i < 10; ++i) testF.submitAnswer(i, 99);
+    EXPECT_EQ(testF.calculateMark(), 'F');
 }
--- MathTestLib/math_test.h
+++ MathTestLib/math_test.h
@@ -1,39 +1,38 @@
 #pragma once
 #include <cstdlib>
 #include <ctime>
-#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <string>
 
 struct Task {
-	int num_1;
-	int num_2;
-	char operation;
-	double answer;
+    int num_1;
+    int num_2;
+    char operation;
+    int answer;
 
-	double calculateAnswer() {
-		double ans = 0;
-		switch (operation) {
-		case '+':
-			ans = num_1 + num_2;
-			break;
-		case '-':
-			ans = num_1 - num_2;
-			break;
-		case '*':
-			ans = num_1 * num_2;
-			break;
-		case '/':
-			ans = static_cast<double>(num_1) / num_2;
-			break;
-		}
-		return ans;
-	}
+    int calculateAnswer() {
+        int ans = 0;
+        switch (operation) {
+        case '+':
+            ans = num_1 + num_2;
+            break;
+        case '-':
+            ans = num_1 - num_2;
+            break;
+        case '*':
+            ans = num_1 * num_2;
+            break;
+        }
+        return ans;
+    }
 
     Task(int min_val = -100, int max_val = 100) {
         int range = max_val - min_val + 1;
         num_1 = (std::rand() % range) + min_val;
         num_2 = (std::rand() % range) + min_val;
 
-        int op_rander = std::rand() % 4;
+        int op_rander = std::rand() % 3;
         switch (op_rander) {
         case 0:
             operation = '+';
@@ -44,31 +43,182 @@ struct Task {
         case 2:
             operation = '*';
             break;
-        case 3:
-            operation = '/';
-            break;
         }
-
-        if (operation == '/' && num_2 == 0) {
-            if (max_val >= 1) {
-                num_2 = 1;
-            }
-            else if (min_val <= -1) {
-                num_2 = -1;
-            }
-            else {
-                operation = '+';
-            }
-        }
-
         answer = calculateAnswer();
     }
 
-	Task(int n1, int n2, char op) : num_1(n1), num_2(n2), operation(op) {
-		answer = calculateAnswer();
-	}
+    Task(int n1, int n2, char op) : num_1(n1), num_2(n2), operation(op) {
+        if (op != '-' && op != '+' && op != '*') {
+            throw std::invalid_argument("ERROR: operation char is not valid!");
+        }
+        answer = calculateAnswer();
+    }
 };
 
 class MathTest {
+private:
+    Task* _tasks;
+    int _counts;
+    int* _user_answers;
+    int _correct_count;
 
+public:
+    MathTest(int count) : _counts(count), _correct_count(0) {
+        if (count <= 0) {
+            throw std::invalid_argument("ERROR: Task count must be bigger than 0!");
+        }
+        _tasks = new Task[_counts];
+        _user_answers = new int[_counts] {0};
+        for (int i = 0; i < _counts; ++i) {
+            _tasks[i] = Task();
+        }
+    }
+
+    MathTest(int count, int min_val, int max_val) : _counts(count), _correct_count(0) {
+        if (count <= 0) {
+            throw std::invalid_argument("ERROR: Task count must be bigger than 0!");
+        }
+        _tasks = new Task[_counts];
+        _user_answers = new int[_counts] {0};
+        for (int i = 0; i < _counts; ++i) {
+            _tasks[i] = Task(min_val, max_val);
+        }
+    }
+
+    MathTest(int count, int min_val, int max_val, char op) : _counts(count), _correct_count(0) {
+        if (count <= 0) {
+            throw std::invalid_argument("ERROR: Task count must be bigger than 0!");
+        }
+        if (op != '-' && op != '+' && op != '*') {
+            throw std::invalid_argument("ERROR: operation char is not valid!");
+        }
+        _tasks = new Task[_counts];
+        _user_answers = new int[_counts] {0};
+        for (int i = 0; i < _counts; ++i) {
+            _tasks[i] = Task(min_val, max_val);
+            _tasks[i].operation = op;
+            _tasks[i].answer = _tasks[i].calculateAnswer();
+        }
+    }
+
+    ~MathTest() {
+        delete[] _tasks;
+        delete[] _user_answers;
+    }
+
+    int getCounts() const noexcept {
+        return _counts;
+    }
+    int getCorrectCount() const noexcept {
+        return _correct_count;
+    }
+    Task getTask(int index) const {
+        return _tasks[index];
+    }
+    int getUserAnswer(int index) const {
+        return _user_answers[index];
+    }
+
+    void setTask(int index, const Task& task) {
+        if (index >= 0 && index < _counts) {
+            _tasks[index] = task;
+        }
+        else {
+            throw std::out_of_range("ERROR: index out of range!");
+        }
+    }
+
+    void submitAnswer(int index, int answer) {
+        if (index < 0 || index >= _counts) {
+            throw std::out_of_range("ERROR: index out of range!");
+        }
+
+        _user_answers[index] = answer;
+
+        if (_tasks[index].answer == answer) {
+            _correct_count++;
+        }
+    }
+
+    char calculateMark() const {
+        double percentage = (static_cast<double>(_correct_count) / _counts) * 100.0;
+        if (percentage >= 90.0) {
+            return 'A';
+        }
+        if (percentage >= 80.0) {
+            return 'B';
+        }
+        if (percentage >= 70.0) {
+            return 'C';
+        }
+        if (percentage >= 60.0) {
+            return 'D';
+        }
+        return 'F';
+    }
+
+    void run() {
+        std::cout << "ћатематический тест на " << _counts << " вопросов:" << std::endl;
+        for (int i = 0; i < _counts; ++i) {
+            std::cout << "¬опрос " << (i + 1) << ":" << std::endl
+                << _tasks[i].num_1 << " " << _tasks[i].operation << " " << _tasks[i].num_2 << " = ";
+
+            int ans; 
+            std::cin >> ans;
+
+            submitAnswer(i, ans);
+        }
+        std::cout << std::endl << "“ест завершен!" << std::endl << std::endl;
+        show_statistics();
+    }
+
+    void show_statistics() const {
+        const int col_width = 9;
+
+        //1: номера вопросов
+        std::cout << "|" << std::setw(12) << "No" << " |";
+        for (int i = 0; i < _counts; ++i) {
+            std::cout << std::setw(col_width) << (i + 1) << " |";
+        }
+        std::cout << "\n";
+
+        //разделитель динамической длины
+        int total_width = 15 + (col_width + 2) * _counts;
+        std::cout << "+" << std::string(total_width - 2, '-') << "+\n";
+
+        //2: вопросы
+        std::cout << "|" << std::setw(12) << "Question" << " |";
+        for (int i = 0; i < _counts; ++i) {
+            std::string q_str = std::to_string(_tasks[i].num_1) + " " +
+                _tasks[i].operation + " " +
+                std::to_string(_tasks[i].num_2);
+            std::cout << std::setw(col_width) << q_str << " |";
+        }
+        std::cout << "\n";
+
+        //3: ответы правильные
+        std::cout << "|" << std::setw(12) << "True Answer" << " |";
+        for (int i = 0; i < _counts; ++i) {
+            std::cout << std::setw(col_width) << _tasks[i].answer << " |";
+        }
+        std::cout << std::endl;
+
+        //4: ответы пользовател€
+        std::cout << "|" << std::setw(12) << "Your Answer" << " |";
+        for (int i = 0; i < _counts; ++i) {
+            std::cout << std::setw(col_width) << _user_answers[i] << " |";
+        }
+        std::cout << "\n";
+
+        //5: результат
+        std::cout << "|" << std::setw(12) << "Result" << " |";
+        for (int i = 0; i < _counts; ++i) {
+            char res = (_tasks[i].answer == _user_answers[i]) ? '+' : '-';
+            std::cout << std::setw(col_width) << res << " |";
+        }
+        std::cout << std::endl << std::endl;
+
+        std::cout << "Total Result: " << _correct_count << " / " << _counts
+            << " (mark: " << calculateMark() << ")" << std::endl;
+    }
 };
